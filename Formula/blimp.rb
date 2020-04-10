30cda077d2782c97213b1a77a4179c579067835b
Jordan Moore <crikket.007@gmail.com>
2020-04-10 14:00:44 -0500

    add blimp version

--- Formula/blimp.rb
+++ Formula/blimp.rb
@@ -3,6 +3,7 @@ class Blimp < Formula
   homepage "https://kelda.io"
   url "https://blimp-releases.s3-us-west-1.amazonaws.com/blimp-osx-0.5.0"
   sha256 "6719e5df874ecbf1ce3a3b61ba2451bb5ac92a5cd3da9d9676d877e40708e111"
+  version "0.5.0"
 
   def install
     bin.install "blimp-osx-0.5.0"
