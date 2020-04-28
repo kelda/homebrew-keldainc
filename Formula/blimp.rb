2f7e04fbc72af19aea4013183e780c78706e6c16
Kevin Lin <kevin@kelda.io>
2020-04-28 15:15:37 -0700

    Update to version 0.6.7

--- Formula/blimp.rb
+++ Formula/blimp.rb
@@ -1,13 +1,13 @@
 class Blimp < Formula
   desc "CLI for Kelda's Blimp (Docker Compose for the Cloud)"
   homepage "https://kelda.io"
-  url "https://blimp-releases.s3-us-west-1.amazonaws.com/blimp-osx-0.6.0"
-  sha256 "ddc4c447c3e542b8986f799d01a0c2673a138775e327fab445674b185764e439"
-  version "0.6.0"
+  url "https://blimp-releases.s3-us-west-1.amazonaws.com/blimp-osx-0.6.7"
+  sha256 "5aed5e25db93297de3332ab850c963f2587f4c674c395243ca598ad76607b66d"
+  version "0.6.7"
 
   def install
-    bin.install "blimp-osx-0.6.0"
-    mv bin/"blimp-osx-0.6.0", bin/"blimp"
+    bin.install "blimp-osx-0.6.7"
+    mv bin/"blimp-osx-0.6.7", bin/"blimp"
   end
 
   test do
