a66b6da8a04b4847d10829ad89b4c36cb68ce402
Ethan J. Jackson <ethan@kelda.io>
2020-05-06 18:28:56 -0700

    Pull version into a variable to make it easier to update

--- Formula/blimp.rb
+++ Formula/blimp.rb
@@ -1,13 +1,13 @@
 class Blimp < Formula
+  version "0.7.3"
+  sha256 "011881b4579cb375b96731ba6e28c673bd8a6e8058fa272790051dea12b7f6a5"
   desc "CLI for Kelda's Blimp (Docker Compose for the Cloud)"
   homepage "https://kelda.io"
-  url "https://blimp-releases.s3-us-west-1.amazonaws.com/blimp-osx-0.7.3"
-  sha256 "011881b4579cb375b96731ba6e28c673bd8a6e8058fa272790051dea12b7f6a5"
-  version "0.7.3"
+  url "https://blimp-releases.s3-us-west-1.amazonaws.com/blimp-osx-#{version}"
 
   def install
-    bin.install "blimp-osx-0.7.3"
-    mv bin/"blimp-osx-0.7.3", bin/"blimp"
+    bin.install "blimp-osx-#{version}"
+    mv bin/"blimp-osx-#{version}", bin/"blimp"
   end
 
   test do
