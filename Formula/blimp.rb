38aaaa96de9281053b0ad62a23743a91bf61fd40
Kevin Lin <kevin@kelda.io>
2020-05-19 13:44:48 -0700

    Update to version 0.11.10

--- Formula/blimp.rb
+++ Formula/blimp.rb
@@ -1,6 +1,6 @@
 class Blimp < Formula
-  version "0.11.8"
-  sha256 "0cd6415fcbfaed069b4b5d2a3a155781e0f43dfaf73f3271430f33fb37ea71d5"
+  version "0.11.10"
+  sha256 "89a8fdf1fc8d4f093adcf2babe577e5291e4f1b765c01ea103a4b9a961542dd2"
   desc "CLI for Kelda's Blimp (Docker Compose for the Cloud)"
   homepage "https://kelda.io"
   url "https://blimp-releases.s3-us-west-1.amazonaws.com/blimp-osx-#{version}"
